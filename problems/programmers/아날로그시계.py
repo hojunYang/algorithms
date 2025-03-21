4703bfe23ea4ed4d935d102a30f24af4bfe7650b
hojunYang <hojun5104@gmail.com>
2025-03-21 16:10:22 +0900

    1 문제 해결

---
+++ problems/programmers/아날로그시계.py
@@ -0,0 +1,11 @@
+def solution(h1, m1, s1, h2, m2, s2):
+    return 
+
+
+print(solution(0, 5, 30, 0, 7, 0))
+print(solution(12, 0, 0, 12, 0, 30))
+print(solution(0, 6, 1, 0, 6, 6))
+print(solution(11, 59, 30, 12, 0, 0))
+print(solution(11, 58, 59, 11, 59, 0))
+print(solution(1, 5, 5, 1, 5, 6))
+print(solution(0, 0, 0, 23, 59, 59))
